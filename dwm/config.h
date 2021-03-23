ae3361fd7eadd5814e49aeb536f80083f1767052
Artem Shchelkunov <artem@shchelkunov.xyz>
2021-03-23 14:56:15 +0100

    Сделал красивый статусбар

--- dwm/config.h
+++ dwm/config.h
@@ -6,8 +6,8 @@ static const unsigned int gappx     = 4;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 0;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
+static const int topbar             = 1;        /* 0 means bottom bar */
+static const char *fonts[]          = { "monospace:size=10","NotoColorEmoji:pixelsize=10:antialias=true:autohint=true" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -67,13 +67,13 @@ static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
--- dwmblocks/blocks.h
+++ dwmblocks/blocks.h
@@ -1,11 +1,16 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const char *fonts[]          = { "monospace:size=16", "-misc-font Noto Color Emoji:style=Regular"};
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-	{"Bat:", "cat /sys/class/power_supply/BAT0/capacity",	30,		0},
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+
+	{"🧠 ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+	{"📦 ", "checkupdates | wc -l",				86400,		0},
+	{"🔉 ", "pamixer --get-volume",				5,		0},
+	{"🕒 ", "date '+%R'",					60,		0},
+	{"📅 ", "date '+%D %a'",					3600,		0},
+	{"🔋 ", "cat /sys/class/power_supply/BAT0/capacity",	30,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
+static char delim[] = " \0 ";
 static unsigned int delimLen = 5;
